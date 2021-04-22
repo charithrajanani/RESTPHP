9a507dd00c0e208dfe38f0e4a76226d9cb1b0e31
charithrajanani <65007110+charithrajanani@users.noreply.github.com>
2021-04-22 12:40:01 +0530

    Add files via upload

---
+++ initialize.php
@@ -0,0 +1,14 @@
+<?php
+
+defined('DS') ? null : define ('DS', DIRECTORY_SEPERATOR);
+defined('SITE_ROOT') ? null : define('SITE_ROOT', DS . 'wamp64'.DS. 'www' .DS.'phprest');
+defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT.DS. 'include'); 
+defined('CORE_PATH') ? null : define('CORE_PATH', SITE_ROOT.DS. 'core'); 
+
+//load the config file first
+require_once(INC_PATH.DS."config.php");
+
+//core classes
+require_once(CORE_PATH.DS. "post.php");
+
+?>
